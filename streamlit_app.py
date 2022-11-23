36da551975d43f06df64bd27d23908b81044ff5a
U799389 <118804195+U799389@users.noreply.github.com>
2022-11-23 18:24:08 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,7 @@ streamlit.header('Breakfast Menu')
 streamlit.title('Omega 3 & Blueberry Oatmeal')
 streamlit.title('Kale,Spinach & Rocket Smoothie')
 streamlit.title('Hard-Boiled Free-Range Egg')
+
 streamlit.title('My Mom's New Healthy Diner')
 streamlit.header('Breakfast Favorites')
 streamlit.title('🥣Omega 3 & Blueberry Oatmeal')
