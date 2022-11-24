b71b161ecc8100aa74ebaf63ea71d26356572625
U799389 <118804195+U799389@users.noreply.github.com>
2022-11-24 16:25:14 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,3 +14,4 @@ streamlit.header('🍌🥭Build Your Own Fruit Smoothie🥝🍇')
 
 import pandas
 my_fruit_list=pandas.read_csv("https://uni-lab-files.S3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
