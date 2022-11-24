4dbd889c901908eab64590c91484884c6a999af8
U799389 <118804195+U799389@users.noreply.github.com>
2022-11-24 16:23:21 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,3 +11,6 @@ streamlit.title('Hard-Boiled Free-Range Egg')
 streamlit.title('🥑🍞Avocado Toast')
 
 streamlit.header('🍌🥭Build Your Own Fruit Smoothie🥝🍇')
+
+import pandas
+my_fruit_list=pandas.read_csv("https://uni-lab-files.S3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
